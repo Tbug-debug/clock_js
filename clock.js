ea36c2c4c26ccf40d7c1cab2710306b5ec0077ad
Tbug-debug <lhg6325@gmail.com>
2021-01-03 22:05:19 +0900

    clock_js(4)

--- clock.js
+++ clock.js
@@ -1,18 +1,26 @@
+/*html에 class = js-clock과 h1의 정보를 불러옴, "."를 붙이고 안붙이고의 차이는
+클레스명인 것과 아닌 것의 차이가 있다.*/
 const clockContainer = document.querySelector(".js-clock"),
   clockTitle = clockContainer.querySelector("h1");
 
   function getTime(){
+      //날짜와 시간을 불러오는 부분//
       const date = new Date();
       const minutes = date.getMinutes();
       const hours = date.getHours();
       const seconds = date.getSeconds();
+      //사간을 index.html의 h1에 넣는 부분//
+      /*ternary operator(삼항연산자)를 썼다. 이유는 "59"초 다음 "1"이
+      "01"로 나타내주기 위해서 이다.*/
       clockTitle.innerText = `${hours < 10 ? `0${hours}` : hours}:${minutes 
         < 10 ? `0${minutes}` : minutes}:${
           seconds < 10 ? `0${seconds}`: seconds}`;
   }
 
 function init(){
+    //getTime의 함수를 불러옴//
     getTime();
+    //시간을 실시간으로 불러옴, 1초간격으로 불러옴.//
     setInterval(getTime, 1000);
 }
 init();
--- gretting.js
+++ gretting.js
@@ -2,43 +2,63 @@ const form = document.querySelector(".js-form"),
     input = form.querySelector("input"),
     greeting = document.querySelector(".js-greetings");
 
+//로컬 스토리지의 불러온 값을 USER_LS에 저장함.//
 const USER_LS = "currentUser",
-    SHOWING_CN = "showing"
+//css의 class의 showing 값을 SHOWING_CN에 저장함// 
+   SHOWING_CN = "showing"
 
 function saveName(text){
+    //input에 친 이름을 로컬 스토리지에 저장하여 F5를 눌러도 값을 그대로 유지하는 함수//
     localStorage.setItem(USER_LS,text);
 }
 
 function handleSumbit(event){
+    /*input안에 정보를 치고 엔터를 누르면 
+    정보가 사라지는 것을 방지하기 위한 것.*/
     event.preventDefault();
+    //input에 정보를 넣고 엔터를 치면 그 값(value)을 currentValue 안에 저장함.//
     const currentValue = input.value;
+    //input에 정보를 치면 currentValue에 있는 값을 paintingGreeting의 함수로 전달//
     paintGreeting(currentValue);
+    //input에 친 이름을 로컬 스토리지에 저장하여 F5를 눌러도 값을 그대로 유지하는 함수//
     saveName(currentValue)
 }
     
 function askForName(){
+    //로컬 스토리지에 값이 없다면 input으로 이름을 요청함.//
     form.classList.add(SHOWING_CN);
+    //누군가가 input에 이름을 제출하면 그것을 알아차리는 역할.//
     form.addEventListener("submit", handleSumbit)
 }
 
 function paintGreeting(text){
+    //만약 로컬 스토리지에 정보가 있다면 form을 없애줘라//
     form.classList.remove(SHOWING_CN);
+    //만약 로컬 스토리지에 정보가 있다면 화면에 greeting안의 내용을 보여줘라//
     greeting.classList.add(SHOWING_CN);
+    /*만약 로컬 스토리지에 정보가 있다면,
+    greeting 안에 Hello ${text}라는 택스트를 넣어줌.*/
     greeting.innerText = `Hello ${text}`;
 }
 
 function loadName(){
+    //로컬 스토리지에서 유저 정보를 불러와서 currentUser안에 값을 저장함.//
     const currentUser = localStorage.getItem(USER_LS);
+    //로컬 스토리지에 값이 있는지 혹은 없는지 체크하는 if문.//
     if(currentUser === null){
+        //로컬 스토리지에 정보가 없으면 user의 이름을 요청하는 함수//
         askForName();
     }else{
+        /*유저의 이름 값이 주어진 경우, 즉 null이 아닐 경우...
+        로컬 스토리지의 currentUser에 있는 값으로 paintingGreeting
+        함수를 실행시킴*/
         paintGreeting(currentUser);
     }
 }
 
 function init(){
+    //loadName 함수를 실행시킴//
     loadName();
 
 }
-
 init();
